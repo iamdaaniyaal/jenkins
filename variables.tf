5510c40e3a1bf8e7f2828649aaf981a267368f76
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-16 13:22:51 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,5 @@ variable "sonarvpc" {}
 variable "sonarsub" {}
 // variable "elk_instance_name" {}
 variable "instance_name" {}
+variable "jenkins_instance_ip_name" {}
+variable "jenkins_instance_ip_region" {}
